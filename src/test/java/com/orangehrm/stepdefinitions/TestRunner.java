a1576dd5d849442c8d50ea75f84c0706e6629f06
Benakeshwar <benakeshwargk@gmail.com>
2025-09-02 16:41:03 +0530

    Added step definitions + feature files + updated pom

---
+++ src/test/java/com/orangehrm/stepdefinitions/TestRunner.java
@@ -0,0 +1,21 @@
+package com.orangehrm.stepdefinitions;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="C:\\Users\\Benakesh\\git\\repository\\CGI_Aug25\\src\\test\\resources\\Features"
+,glue= {"com.orangehrm.stepdefinitions"}
+
+,dryRun=false
+
+,monochrome=true)
+ 
+ 
+public class TestRunner {
+
+ 
+}
+
+ 
